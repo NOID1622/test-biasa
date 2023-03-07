3e93d43b610a3771dc9ebff404473b276e5600f5
NOID1622 <112671264+NOID1622@users.noreply.github.com>
2023-03-07 10:35:16 +0700

    Add files via upload

---
+++ perkalian menggunakan while.py
@@ -0,0 +1,19 @@
+def perkalian(b,a):
+    hasil = b * a
+    i = 1
+    while i <= hasil  :
+        print(a,'+', end=' ')
+        i += a
+    else:
+        print('=',hasil)
+    
+        
+
+a =  int(input('a:  '))
+b =  int(input('a:  '))
+
+perkalian(a,b)
+
+
+
+
