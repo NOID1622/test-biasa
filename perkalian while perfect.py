e2650c561d3a994a4c8dc140e476f0cb0c783ddd
NOID1622 <112671264+NOID1622@users.noreply.github.com>
2023-03-09 16:50:15 +0700

    Add files via upload

---
+++ perkalian while perfect.py
@@ -0,0 +1,21 @@
+def perkalian(b,a):
+    hasil = b * a-a
+    i = 1
+    print(b,'x',a,'= ',end='')
+    while i <= hasil  :
+        
+        print(a,'+', end=' ')
+        i += a
+    else:
+        print(a,'=',hasil+a)
+    
+        
+
+# a =  int(input('a:  '))
+# b =  int(input('b:  '))
+
+perkalian(6,5)
+perkalian(7,10)
+
+
+
